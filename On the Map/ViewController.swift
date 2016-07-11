52e33ae8df23254657e90b02197fe1af10511804
Rod Musser <rmusser@yahoo.com>
2016-07-10 18:20:23 -0700

    Initial Check in

--- On the Map/ViewController.swift
+++ On the Map/ViewController.swift
@@ -10,14 +10,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textView: UITextView!
+    
+    @IBOutlet weak var signUp: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        configureSignUp()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func configureSignUp() {
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action: "userSignUp:")
+        tapGesture.numberOfTapsRequired = 1
+        signUp.userInteractionEnabled = true
+        signUp.addGestureRecognizer(tapGesture)
+        
+        
+    }
+    
+    func userSignUp(recognizer: UITapGestureRecognizer)  {
+        let url = NSURL(string: "https://www.udacity.com/account/auth#!/signup")!
+        UIApplication.sharedApplication().openURL(url)
     }
 
 
